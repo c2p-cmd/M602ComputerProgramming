95fd39ba38ae2c7c8a96b5f4e5e324f58d8f7c28
Sharan Thakur <sharandt19@live.com>
2024-10-15 17:02:30 +0200

    adds input_output script

---
+++ c++/input_output/input_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "What is your name?" << endl;
+    string user_name;
+    cin >> user_name;
+    cout << "Welcome " << user_name << " to C++ Programming!" << endl;
+    return 0;
+}
