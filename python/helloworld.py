ff33c2562066bc8f3e94b2a09cecd7c00f4cf883
Sharan Thakur <sharandt19@live.com>
2024-10-15 16:53:45 +0200

    creates first python file

---
+++ python/helloworld.py
@@ -0,0 +1,17 @@
+
+"""
+A project outlining basic input-output methods in Python
+--------------------------------------------------------
+"""
+def main():
+    # Print a welcome message
+    print("Hello world!")
+
+    # Ask user for their name
+    user_name = input("What is your name?")
+
+    # Print user_name with welcome message
+    print(f"Welcome {user_name} to Python programming!")
+
+if __name__ == '__main__':
+    main()
