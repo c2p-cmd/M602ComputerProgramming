6760ad7672ad638d062f09f0692c97efa555c36a
Sharan Thakur <sharandt19@live.com>
2024-10-15 16:53:54 +0200

    creates first c++ file

---
+++ c++/helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cout << "What is your name?" << endl;
+    cin >> user_name;
+    cout << "Welcome " << user_name << " to C++ Programming" << endl;
+    return 0;
+}
