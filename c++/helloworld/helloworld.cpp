78a4fc2109961b5c0553a0af3a70ffd42a27ee38
Sharan Thakur <sharandt19@live.com>
2024-10-15 17:02:17 +0200

    simplfies helloworld

--- c++/helloworld/helloworld.cpp
+++ c++/helloworld/helloworld.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    string user_name;
-    cout << "What is your name?" << endl;
-    cin >> user_name;
-    cout << "Welcome " << user_name << " to C++ Programming" << endl;
+    cout << "Hello World!" << endl;
     return 0;
 }
