f948127d4abb041ddcfb27b2055db4f0a7e6c524
Sharan Thakur <sharandt19@live.com>
2024-10-18 14:20:28 +0200

    adds source code for counting up cpp & py

---
+++ c++/count_up/count_up.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// for measuring the time
+#include <chrono>
+
+int main()
+{
+    // locking namespace to local scope
+    using std::cout;
+    using std::endl;
+
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
+    using std::chrono::nanoseconds;
+
+    auto t1 = high_resolution_clock::now();
+    const int MAX_COUNT = 10000000;
+    int value = 0;
+    for (int i = 0; i < MAX_COUNT; i++)
+    {
+        value++;
+    }
+    auto t2 = high_resolution_clock::now();
+
+    nanoseconds time_taken_ns = t2 - t1;
+    milliseconds time_taken_ms = duration_cast<milliseconds>(time_taken_ns);
+
+    cout << endl << "Counted till " << value << " in " << time_taken_ms.count() << "ms." << endl;
+    return 0;
+}
---
+++ python/count_up.py
@@ -0,0 +1,17 @@
+from timeit import default_timer as timer
+from datetime import timedelta
+
+def main() -> None:
+    start = timer()
+    MAX_COUNT = 1_00_00_000
+
+    value: int = 0
+    for _ in range(MAX_COUNT):
+        value += 1
+    end = timer()
+
+    time_taken = timedelta(seconds=end-start)
+    print(f"Counted till {value} in {time_taken}")
+
+if __name__ == '__main__':
+    main()
