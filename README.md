0858782d477ca5530acf09dcc0b308c85a20f0b0
Sharan Thakur <sharandt19@live.com>
2024-10-22 22:11:38 +0200

    adds operators implementation in `C++`

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # M602 Computer Programming Coding
 
+### Task of "Write 5 different expressions using different operators in C++ and Python"
+- In Python [link](./python/operators.ipynb)
+- In C++ [link](./c++/operators/operators_demo.cpp)
+C++ Operators Output
+![C++ Operators Output](./c++/operators/operators.png)
+
 ### Task of counting to 1,000,000,000 in both c++ and python [video](https://www.youtube.com/watch?v=VioxsWYzoJk)
 
 #### C++ (with `-march=native`) took 764ms [code](./c++/count_up/count_up.cpp)
---
+++ c++/operators/operators_demo.cpp
@@ -0,0 +1,49 @@
+/*
+Today is all about Operators
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Comparational
+6. Logical
+7. Ternary operator
+*/
+#include <iostream>
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    // expression-1
+    cout << "4 + 5 * 6 - 1: " << 4 + 5 * 6 - 1 << endl;
+
+    // expression-2
+    cout << "25 / 6: " << 25 / 6 << endl;
+
+    // expression-3
+    int n1, n2;
+
+    cout << "Enter number 1 ";
+    cin >> n1;
+    cout << endl;
+
+    cout << "Enter number 2: ";
+    cin >> n2;
+
+    cout << "n1(" << n1 << ") > " << "n2(" << n2 << ") = " << (n1 > n2) << endl;
+
+    // expression-4
+    cout << "n1(" << n1 << ") < " << "n2(" << n2 << ") = " << (n1 < n2) << endl;
+
+    // expression-5
+    int x = 9;
+    int y = x++;
+    cout << "x: " << x << " x++: " << y << endl;
+
+    // expressional-6
+    cout << "Ternary Operator: (bool ? true_output : false_output) (9 > 7 ? cool : woah) " << (9 > 7 ? "cool" : "woah") << endl;
+
+    return 0;
+}
